8c5f610f7962b0cf41b8298d475dca9df783c89c
telisik8 <59242165+telisik8@users.noreply.github.com>
2019-12-30 20:55:40 +0700

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,37 @@
+	
+name	"Telisik8"
+short_name	"Telisik8"
+start_url	"https://telisik8.com/"
+display	"standalone"
+background_color	"#fff"
+theme_color	"#C51162"
+description	"Informasi Faktual dan Berimbang"
+icons	
+0	
+src	"https://1.bp.blogspot.com/-yBvRFSPSQOs/XghA8AWkLPI/AAAAAAAAAIU/R7-jLB2nSOA5IVFHWVK790avYGdnqGMAwCEwYBhgL/s48/logoT8.png"
+sizes	"48x48"
+type	"image/png"
+1	
+src	"https://1.bp.blogspot.com/-yBvRFSPSQOs/XghA8AWkLPI/AAAAAAAAAIU/R7-jLB2nSOA5IVFHWVK790avYGdnqGMAwCEwYBhgL/s72/logoT8.png"
+sizes	"72x72"
+type	"image/png"
+2	
+src	"https://1.bp.blogspot.com/-yBvRFSPSQOs/XghA8AWkLPI/AAAAAAAAAIU/R7-jLB2nSOA5IVFHWVK790avYGdnqGMAwCEwYBhgL/s96/logoT8.png"
+sizes	"96x96"
+type	"image/png"
+3	
+src	"https://1.bp.blogspot.com/-yBvRFSPSQOs/XghA8AWkLPI/AAAAAAAAAIU/R7-jLB2nSOA5IVFHWVK790avYGdnqGMAwCEwYBhgL/s144/logoT8.png"
+sizes	"144x144"
+type	"image/png"
+4	
+src	"https://1.bp.blogspot.com/-yBvRFSPSQOs/XghA8AWkLPI/AAAAAAAAAIU/R7-jLB2nSOA5IVFHWVK790avYGdnqGMAwCEwYBhgL/s168/logoT8.png"
+sizes	"168x168"
+type	"image/png"
+5	
+src	"https://1.bp.blogspot.com/-yBvRFSPSQOs/XghA8AWkLPI/AAAAAAAAAIU/R7-jLB2nSOA5IVFHWVK790avYGdnqGMAwCEwYBhgL/s192/logoT8.png"
+sizes	"192x192"
+type	"image/png"
+6	
+src	"https://1.bp.blogspot.com/-yBvRFSPSQOs/XghA8AWkLPI/AAAAAAAAAIU/R7-jLB2nSOA5IVFHWVK790avYGdnqGMAwCEwYBhgL/s512/logoT8.png"
+sizes	"512x512"
+type	"image/png"
